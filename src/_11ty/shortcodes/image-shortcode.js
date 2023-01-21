8da1cfe6d9ef4ee170809ee44f42babb745e1daf
Matthew <97437155+moritznowi@users.noreply.github.com>
2023-01-21 22:49:18 +0100

    Update image-shortcode.js

--- src/_11ty/shortcodes/image-shortcode.js
+++ src/_11ty/shortcodes/image-shortcode.js
@@ -3,7 +3,7 @@ const Image = require('@11ty/eleventy-img');
 async function imageShortcode (src, alt, sizes, classes, loading = 'lazy') {
   let metadata = await Image(src, {
     widths: [25, 320, 640, 960, 1200, 1800, 2400],
-    formats: ['webp', 'jpeg'],
+    formats: ['webp', 'jpeg', 'jpg'],
     urlPath: '/assets/img/',
     outputDir: '_site/assets/img/',
   });
